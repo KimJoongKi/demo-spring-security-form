e612439020e2e4f006cb5d035ae2273505b85025
KimJoongKi <thoug12@naver.com>
2021-03-05 08:58:32 +0900

    Initial commit

---
+++ src/test/java/me/yaddoong/demospringsecurityform/DemoSpringSecurityFormApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yaddoong.demospringsecurityform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringSecurityFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
