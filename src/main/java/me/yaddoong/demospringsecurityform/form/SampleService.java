2ecf2c89f48d07b5dc403ee9692c9f06bddfb4b3
KimJoongKi <thoug12@naver.com>
2021-03-10 08:45:54 +0900

    ThreadLocal

--- src/main/java/me/yaddoong/demospringsecurityform/form/SampleService.java
+++ src/main/java/me/yaddoong/demospringsecurityform/form/SampleService.java
@@ -1,5 +1,7 @@
 package me.yaddoong.demospringsecurityform.form;
 
+import me.yaddoong.demospringsecurityform.account.Account;
+import me.yaddoong.demospringsecurityform.account.AccountContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,11 +11,10 @@ import java.util.Collection;
 
 @Service
 public class SampleService {
+
     public void dashboard() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Object credentials = authentication.getCredentials();
-        boolean authenticated = authentication.isAuthenticated();
+        Account account = AccountContext.getAccount();
+        System.out.println("================");
+        System.out.println("account.getUsername() = " + account.getUsername());
     }
 }
