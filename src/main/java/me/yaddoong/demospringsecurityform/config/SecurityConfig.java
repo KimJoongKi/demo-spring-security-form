34010f24e64980ce56393ea701c90d08d1d7cccb
KimJoongKi <thoug12@naver.com>
2021-03-11 12:56:27 +0900

    졸려졸려

--- src/main/java/me/yaddoong/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/me/yaddoong/demospringsecurityform/config/SecurityConfig.java
@@ -3,13 +3,14 @@ package me.yaddoong.demospringsecurityform.config;
 import me.yaddoong.demospringsecurityform.account.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
